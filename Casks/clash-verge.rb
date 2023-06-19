2c34fddb3e8875be7ab16d41746d1fa8798c2316
nb5p <nb5p@users.noreply.github.com>
2023-06-19 23:35:24 +0800

    add: cask clash-verge

---
+++ Casks/clash-verge.rb
@@ -0,0 +1,21 @@
+cask "clash-verge" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "1.3.2"
+  sha256 arm:   "c4587c7e2111fa05331513abbc7e5bd47a7ef2c210482fe832fa2609bd674098",
+         intel: "e28e6c29cbfa4bb0b5982e8df63ffd90e666a9a7355f6bda44e2e5aa00a9aed"
+
+  url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
+  name "Clash Verge"
+  desc "A Clash GUI based on tauri. Supports Windows, macOS and Linux."
+  homepage "https://github.com/zzzgydi/clash-verge/"
+
+  livecheck do
+    url: url
+    strategy: github_latest
+  end
+
+  auto_updates true
+
+  app "Clash Verge.app"
+end
