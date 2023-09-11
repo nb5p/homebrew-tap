54c317e46539925365b857a670f3042c6313282e
nb5p <nb5p@users.noreply.github.com>
2023-09-11 19:33:00 +0800

    clash-verge 1.3.7

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,9 +1,9 @@
 cask "clash-verge" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.3.6"
-  sha256 arm:   "6e6b48fbb5bbf230001a6bae9e6f82771a9f6235a55d72d98781015678cf4cd8",
-         intel: "5b00ab180c0b1e8067c6dba298e637141001cf634f14cb95eddbd6367e568f3b"
+  version "1.3.7"
+  sha256 arm:   "dbf2b5cbb75e833a6e1b6e57c448afc8b03ec60937fcf79766b4697ed7fdf1c2",
+         intel: "f9c63c38fbbcabb385f0a073813577a9c3eba7134926b381c2f129ec40e578ff"
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash Verge"
